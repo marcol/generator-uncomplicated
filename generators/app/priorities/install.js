eb7504d5c951430a2a004840a823091786ac830e
Marco Dias Lopes <rhino@pacodacomenda.com>
2020-08-08 21:09:23 +0200

    build(yarn): replace npm with yarn
    - Replace npm commands with yarn.
    - Change number of admited lines per function.

--- generators/app/priorities/install.js
+++ generators/app/priorities/install.js
@@ -19,6 +19,6 @@ module.exports = function (gen) {
   }
 
   // install npm dependencies
-  gen.npmInstall(deps.dependencies, { save: true, skipMessage: true })
-  gen.npmInstall(deps.devDependencies, { 'save-dev': true, skipMessage: true })
+  gen.yarnInstall(deps.dependencies, { save: true, skipMessage: true })
+  gen.yarnInstall(deps.devDependencies, { 'save-dev': true, skipMessage: true })
 }
